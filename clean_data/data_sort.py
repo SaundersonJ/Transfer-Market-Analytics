215ac923aab47b1870fcf8b2a810bf99bb4bf701
Joseph Saunderson <06saundersonj@gmail.com>
2025-08-02 18:14:38 -0500

    Update

--- clean_data/data_sort.py
+++ clean_data/data_sort.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 def RemoveColumsNaN(data, columns_to_remove):
     data_cleaned = data.drop(columns=columns_to_remove)
-    new_order = ['Age', 'time', 'xA', 'xG', 'Value']
+    new_order = ["name",'Age', 'time', 'xA', 'xG', 'Value']
     return data_cleaned[new_order].dropna()
 
 def ConvertPrice(dataframe, outfile):
@@ -27,7 +27,9 @@ def ConvertPrice(dataframe, outfile):
 
 #opens file, lists columns to remove, calls function to remove columns and rows with NaN values and sends this data frame into a function that formats the price correctly and then writes it to a txt file with each record on a single line with each stat separated by a space
 merged = pd.read_csv("merged_players.csv")
-columns_to_remove = ["name", "Citizenship", "Contract expiration", "DOB", "Height (m)", "ID", "Joined", "Last club", "Market value history", "Nationality", "Other positions", "Position", "Since", "Team", "Transfer history", "Value last updated", "assists", "games", "goals", "id", "key_passes", "npg", "npxG", "position", "red_cards", "shots", "source", "team", "team_title", "xGBuildup", "xGChain", "yellow_cards"]
+#columns_to_remove = ["name", "Citizenship", "Contract expiration", "DOB", "Height (m)", "ID", "Joined", "Last club", "Market value history", "Nationality", "Other positions", "Position", "Since", "Team", "Transfer history", "Value last updated", "assists", "games", "goals", "id", "key_passes", "npg", "npxG", "position", "red_cards", "shots", "source", "team", "team_title", "xGBuildup", "xGChain", "yellow_cards"]
+columns_to_remove = ["Citizenship", "Contract expiration", "DOB", "Height (m)", "ID", "Joined", "Last club", "Market value history", "Nationality", "Other positions", "Position", "Since", "Team", "Transfer history", "Value last updated", "assists", "games", "goals", "id", "key_passes", "npg", "npxG", "position", "red_cards", "shots", "source", "team", "team_title", "xGBuildup", "xGChain", "yellow_cards"]
+
 
 #print(merged)
 merged_text_outfile = open("merged_format_final.txt", 'w')
