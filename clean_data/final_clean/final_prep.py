1c2bea7273f9d1d6fc880bde30651238f2cc0a26
Joseph Saunderson <06saundersonj@gmail.com>
2025-08-04 13:03:13 -0500

    Added the final prep files that determine price ranges to class labels

---
+++ clean_data/final_clean/final_prep.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+data = np.loadtxt("merged_players.txt")
+
+prices = data[:,-1]
+print(f'mean: {np.mean(prices)}')
+print(f'median: {np.median(prices)}')
+print(f'min: {np.min(prices)}')
+print(f'max: {np.max(prices)}')
+print()
+# Generate evenly spaced points
+points = np.linspace(np.min(prices), np.max(prices), 30 + 1)
+
+# Create the ranges
+ranges = []
+for i in range(30):
+    ranges.append((points[i], points[i+1]))
+
+print(ranges)
+
+for i in range(len(prices)):
+    for lower, upper in ranges:
+        if lower <= prices[i] < upper: # Check if the value falls within the current range
+            data[i,-1] = int((lower + upper) / 2)
+            
+np.savetxt('merged_players_final.txt', data, fmt='%d')
